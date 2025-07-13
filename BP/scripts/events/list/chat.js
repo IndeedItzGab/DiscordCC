5214ea9189df8989a1702b8232e23876e4726141
New Username <you@example.com>
2025-07-13 14:41:34 +0800

    V1.0.4

--- BP/scripts/events/list/chat.js
+++ BP/scripts/events/list/chat.js
@@ -4,7 +4,7 @@ import { config } from "../../config.js"
 
 world.afterEvents.chatSend.subscribe((event) => {
   if(!config.token || !config.channel) return;
-  send({username: event.sender.name, message: {content: event.message}})
+  send({username: event.sender.name, message: {content: event.message}, avatarUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fpin%2F375487687673255002%2F&psig=AOvVaw2B-al6kKU_QRqwHemEjULK&ust=1752473301217000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKDnlvmVuY4DFQAAAAAdAAAAABAE"})
 })
 
 export async function send(data = JSON.parse(data)) {
@@ -36,5 +36,4 @@ export async function send(data = JSON.parse(data)) {
   })
   
   const res = await http.request(res2)
-  console.info(res.status)
 }
--- BP/scripts/events/list/died.js
+++ BP/scripts/events/list/died.js
@@ -4,162 +4,149 @@ import { config } from "../../config.js"
 
 world.afterEvents.entityDie.subscribe(event => {
   if(!config.token || !config.channel) return;
-  if(event.deadEntity.typeId !== "minecraft:player") return;
-  
+  if(event.deadEntity.typeId === "minecraft:player" || event.deadEntity.getComponent("minecraft:is_tamed") || event.deadEntity.nameTag) {
+     const cause = event.damageSource
 
-  const cause = event.damageSource
+    let message;
+    const killer = cause.damagingEntity?.name || cause.damagingEntity?.typeId.split(':').slice(1).join(" ").split("_").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
+    const victim = event.deadEntity.name || event.deadEntity.nameTag || event.deadEntity.typeId.split(':').slice(1).join(" ").split("_").map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
+    switch(cause.cause) {
+      case "anvil":
+        message = `${victim} was squashed by a falling anvil`
+        break;
+      case "blockExplosion":
+        message = `${victim} blew up`
+        break;
+      case "campfire":
+        message = `${victim} went up in flames`
+        break;
+      case "contact":
+        if(cause.damagingEntity) {
+          message = `${victim} was slain by ${killer}`
+          
+        } else {
+          message = `${victim} was pricked to death`
+        }  
+        break;
+      case "drowning":
+        message = `${victim} drowned`
+        break;
+      case "entityAttack":
+        message = `${victim} was slain by ${killer}`
+        break;
+      case "entityExplosion":
+        if(cause.damagingEntity?.typeId === "minecraft:tnt") {
+          message = `${victim} was blown up by Block of TNT`
+        } else {
+            message = `${victim} was blown up by ${killer}`
+        }
+        break;
+      case "fall":
+        message = `${victim} fell from a high place`
+        break;
+      case "fallingBlock":
+        message = `${victim} was squashed by a falling block`
+        break;
+      case "fire":
+        message = `${victim} went up in flames`
+        break;
+      case "fireTick":
+        message = `${victim} burned to death`
+        break;
+      case "fireworks":
+        message = `${victim} went off with a bang`
+        break;
+      case "flyIntoWall":
+        message = `${victim} experienced kinetic energy`
+        break;
+      case "freezing":
+        message = `${victim} froze to death`
+        break;
+      case "lava":
+        message = `${victim} tried to swim in lava`
+        break;
+      case "lightning":
+        message = `${victim} was struck by lightning`
+        break;
+      case "maceSmash":
+        message = `${victim} was smashed by ${killer}`
+        break;
+      case "magic":
+        if(cause.damagingEntity) {
+          message `${victim} was killed by ${killer} using magic`
+        } else {
+          message = `${victim} was killed by magic`
+        }
+        break;
+      case "magma":
+        message = `${victim} discovered floor was lava`
+        break;
+      case "none":
+        message = `${victim} died`
+        break;
+      case "override":
+        message = `${victim} died`
+        break;
+      case "projectile":
+        if(!cause.damagingEntity && cause.damagingProjectile?.typeId === "minecraft:arrow") {
+          message = `${victim} was slain by Arrow`
+        } else if(cause.damagingEntity && cause.damagingProjectile?.typeId === "minecraft:arrow") {
+          message = `${victim} was shot by ${killer}`
 
-  let message;
-  switch(cause.cause) {
-    case "anvil":
-      message = `${event.deadEntity.name} was squashed by a falling anvil`
-      break;
-    case "blockExplosion":
-      message = `${event.deadEntity.name} blew up`
-      break;
-    case "campfire":
-      message = `${event.deadEntity.name} went up in flames`
-      break;
-    case "contact":
-      if(cause.damagingEntity) {
-        message = `${event.deadEntity.name} was slain by ${cause.damagingEntity?.typeId.split(':').slice(1).join(" ").split("_")
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(" ")}`
-        
-      } else {
-        message = `${event.deadEntity.name} was pricked to death`
-      }  
-      break;
-    case "drowning":
-      message = `${event.deadEntity.name} drowned`
-      break;
-    case "entityAttack":
-      message = `${event.deadEntity.name} was slain by ${cause.damagingEntity?.typeId.split(':').slice(1).join(" ").split("_")
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(" ")}`
-      break;
-    case "entityExplosion":
-      if(cause.damagingEntity?.typeId === "minecraft:tnt") {
-        message = `${event.deadEntity.name} was blown up by Block of TNT`
-      } else {
-          message = `${event.deadEntity.name} was blown up by ${cause.damagingEntity?.typeId.split(':').slice(1).join(" ").split("_")
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(" ")}`
-      }
-      break;
-    case "fall":
-      message = `${event.deadEntity.name} fell from a high place`
-      break;
-    case "fallingBlock":
-      message = `${event.deadEntity.name} was squashed by a falling block`
-      break;
-    case "fire":
-      message = `${event.deadEntity.name} went up in flames`
-      break;
-    case "fireTick":
-      message = `${event.deadEntity.name} burned to death`
-      break;
-    case "fireworks":
-      message = `${event.deadEntity.name} went off with a bang`
-      break;
-    case "flyIntoWall":
-      message = `${event.deadEntity.name} experienced kinetic energy`
-      break;
-    case "freezing":
-      message = `${event.deadEntity.name} froze to death`
-      break;
-    case "lava":
-      message = `${event.deadEntity.name} tried to swim in lava`
-      break;
-    case "lightning":
-      message = `${event.deadEntity.name} was struck by lightning`
-      break;
-    case "maceSmash":
-       message = `${event.deadEntity.name} was smashed by ${cause.damagingEntity?.typeId.split(':').slice(1).join(" ").split("_")
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(" ")}`
-      break;
-    case "magic":
-      if(cause.damagingEntity) {
-        message `${event.deadEntity.name} was killed by ${cause.damagingEntity?.typeId.split(':').slice(1).join(" ").split("_")
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(" ")} using magic`
-      } else {
-        message = `${event.deadEntity.name} was killed by magic`
-      }
-      break;
-    case "magma":
-      message = `${event.deadEntity.name} discovered floor was lava`
-      break;
-    case "none":
-      message = `${event.deadEntity.name} died`
-      break;
-    case "override":
-      message = `${event.deadEntity.name} died`
-      break;
-    case "projectile":
-      if(!cause.damagingEntity && cause.damagingProjectile?.typeId === "minecraft:arrow") {
-        message = `${event.deadEntity.name} was slain by Arrow`
-      } else if(cause.damagingEntity && cause.damagingProjectile?.typeId === "minecraft:arrow") {
-        message = `${event.deadEntity.name} was shot by ${cause.damagingEntity.typeId.split(':').slice(1).join(" ").split("_")
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(" ")}`
+        }
+        break;
+      case "selfDestruct":
+        message = `${victim} died`
+        break;
+      case "sonicBoom":
+        message = `${victim} was obliterated by a sonically-charged shriek whilst trying to escape ${killer}`
+        break;
+      case "soulCampfire":
+        message = `${victim} went up in flames`
+        break;
+      case "stalactite":
+        message = `${victim} was skewered by a falling stalactite`
+        break;
+      case "stalagmite":
+        message = `${victim} was impaled on a stalagmite`
+        break;
+      case "starve":
+        message = `${victim} starved to death`
+        break;
+      case "suffocation":
+        message = `${victim} suffocated in a wall`
+        break;
+      case "thorns":
+        message = `${victim} was killed trying to hurt ${killer}`
+        break;
+      case "void":
+        message = `${victim} fell out of the world`
+        break;
+      case "wither":
+        message = `${victim} withered away`
+        break;
+      default:
+        message = `${victim} died`
+        break;
+    }
 
-      }
-      break;
-    case "selfDestruct":
-      message = `${event.deadEntity.name} died`
-      break;
-    case "sonicBoom":
-      message = `${event.deadEntity.name} was obliterated by a sonically-charged shriek whilst trying to escape ${cause.damagingEntity.typeId.split(':').slice(1).join(" ").split("_")
-            .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-            .join(" ")}`
-      break;
-    case "soulCampfire":
-      message = `${event.deadEntity.name} went up in flames`
-      break;
-    case "stalactite":
-      message = `${event.deadEntity.name} was skewered by a falling stalactite`
-      break;
-    case "stalagmite":
-      message = `${event.deadEntity.name} was impaled on a stalagmite`
-      break;
-    case "starve":
-      message = `${event.deadEntity.name} starved to death`
-      break;
-    case "suffocation":
-      message = `${event.deadEntity.name} suffocated in a wall`
-      break;
-    case "thorns":
-      message = `${event.deadEntity.name} was killed trying to hurt ${cause.damagingEntity?.name}`
-      break;
-    case "void":
-      message = `${event.deadEntity.name} fell out of the world`
-      break;
-    case "wither":
-      message = `${event.deadEntity.name} withered away`
-      break;
-    default:
-      message = `${event.deadEntity.name} died`
-      break;
+    const res2 = new HttpRequest(config.webhook)
+    res2.method = HttpRequestMethod.Post
+    res2.headers = [
+      new HttpHeader("Content-Type", `application/json`)
+    ]
+    res2.body = JSON.stringify({
+      embeds: [
+        {
+          author: {
+            name: message,
+            icon_url: "https://cdn.discordapp.com/attachments/1136508489989111901/1383302370489794772/54f4b55a59ff9ddf2a2655c7f35e4356.jpg?ex=684e4c39&is=684cfab9&hm=e5da707d1ba28406db35d8f3c03db56beeed9b644908d5a54f3f9323213d1bdc&"
+          },
+          color: 0x000000
+        }  
+      ],
+    })
+    
+    http.request(res2)
   }
-
-  const res2 = new HttpRequest(config.webhook)
-  res2.method = HttpRequestMethod.Post
-  res2.headers = [
-    new HttpHeader("Content-Type", `application/json`)
-  ]
-  res2.body = JSON.stringify({
-    embeds: [
-      {
-        author: {
-          name: message,
-          icon_url: "https://cdn.discordapp.com/attachments/1136508489989111901/1383302370489794772/54f4b55a59ff9ddf2a2655c7f35e4356.jpg?ex=684e4c39&is=684cfab9&hm=e5da707d1ba28406db35d8f3c03db56beeed9b644908d5a54f3f9323213d1bdc&"
-        },
-        color: 0x000000
-      }  
-    ],
-  })
-  
-  http.request(res2)
 })
